9ce1db5067f56b173bcdac00e55482ce7bb59399
横井一樹 <yokoikazuki@yokoikazuki.local>
2015-02-28 09:15:50 +0900

    Initial Commit

---
+++ ClapBeat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ClapBeat
+//
+//  Created by 横井一樹 on 2015/02/28.
+//  Copyright (c) 2015年 LifeisTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
